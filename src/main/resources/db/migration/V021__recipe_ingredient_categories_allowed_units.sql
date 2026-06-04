2dbe27bc911c7bf493e3728da49f5c5868dc5b91
nia-pencheva <petralewis.art@gmail.com>
2026-06-04 15:55:17 +0300

    Units: Added cascading delete on records from recipe and warehouse allowed units tables when a unit is deleted

--- src/main/resources/db/migration/V021__recipe_ingredient_categories_allowed_units.sql
+++ src/main/resources/db/migration/V021__recipe_ingredient_categories_allowed_units.sql
@@ -5,7 +5,7 @@ CREATE TABLE recipe_ingredient_categories_allowed_units (
     PRIMARY KEY (id),
     UNIQUE KEY uq_ricu (recipes_ingredient_category_id, unit_id),
     CONSTRAINT fk_ricu_recipe_ingredient_category_id FOREIGN KEY (recipes_ingredient_category_id) REFERENCES recipe_ingredient_categories (id),
-    CONSTRAINT fk_ricu_unit_id FOREIGN KEY (unit_id) REFERENCES units (id)
+    CONSTRAINT fk_ricu_unit_id FOREIGN KEY (unit_id) REFERENCES units (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ${dbName}.recipe_ingredient_categories_allowed_units TO '${appUser}'@'%';
