2cccb2f679ccd23795b387096ab57fcd43035a61
nia-pencheva <petralewis.art@gmail.com>
2026-06-11 11:27:54 +0300

    DB Migrations: Added users to test data seed migration

--- src/main/resources/db/migration/V039__create_admin.sql
+++ src/main/resources/db/migration/V039__create_admin.sql
@@ -2,4 +2,4 @@ INSERT INTO users (email, phone_number, password, password_changed_at, username,
     ('${adminEmail}', '${adminPhoneNumber}', '${adminPassword}', CURRENT_TIMESTAMP, '${adminUsername}', 'Admin');
 
 INSERT INTO users_roles (user_id, role_id) VALUES
-    (1, 1)
+    (1, 1);
